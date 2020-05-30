2eb12e3b61529ef7761195321234a83ddd53c6e4
Rishikesh <contactrshikeshhere@gmail.com>
2020-05-30 11:11:13 +0530

    Updates

---
+++ precache-manifest.d3aea2f150de48bb76d9e34fb2897ad9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9a4343fd428ff2896819207c56100f9",
+    "url": "/SmartBrain/index.html"
+  },
+  {
+    "revision": "151eaf260684c0d5ee5c",
+    "url": "/SmartBrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "05b596db366da18a679a",
+    "url": "/SmartBrain/static/css/main.09280c4d.chunk.css"
+  },
+  {
+    "revision": "151eaf260684c0d5ee5c",
+    "url": "/SmartBrain/static/js/2.61848096.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/SmartBrain/static/js/2.61848096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05b596db366da18a679a",
+    "url": "/SmartBrain/static/js/main.af1d0985.chunk.js"
+  },
+  {
+    "revision": "7d1d471acecf660bda88",
+    "url": "/SmartBrain/static/js/runtime-main.cf057ed3.js"
+  }
+]);
